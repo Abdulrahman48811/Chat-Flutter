f6c17f7ad442cf9801579fb27f81050e32d45a63
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-03-28 17:08:52 -0700

    created homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_flutter/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: ,
+      home: HomeScreen(),
     );
   }
 }
