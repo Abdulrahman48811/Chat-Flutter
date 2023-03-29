34cbddc2034cd00b7b07f690b0dd4dbf815f9150
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-03-29 14:13:49 -0700

    getting rid of stripe & start with ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:chat_flutter/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
