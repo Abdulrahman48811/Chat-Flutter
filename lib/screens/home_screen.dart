fd446994691e59e43141d1e532d5cd4685206d71
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-03-31 23:23:42 -0700

    completed nav bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,8 +4,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Inbox'),
+        elevation: 13,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Inbox',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Container(
         child: Text('Test'),
