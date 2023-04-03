15e348affae77f65c65da0020f372f3285c1babd
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-02 23:05:37 -0700

    created first chat

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,37 +26,95 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 15,
-        ),
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(2),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(40)),
-                border: Border.all(
-                  width: 2,
-                  color: Colors.teal,
-                ),
-                // shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                  ),
-                ],
-              ),
-              child: CircleAvatar(
-                radius: 35,
-                backgroundImage: AssetImage('assets/images/ironman.jpeg'),
-              ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
             ),
-          ],
-        ),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.teal,
+                    ),
+                    // shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: CircleAvatar(
+                    radius: 35,
+                    backgroundImage: AssetImage('assets/images/ironman.jpeg'),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.65,
+                  padding: EdgeInsets.only(
+                    left: 20,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Iron Man',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '12:20pm',
+                            style: TextStyle(
+                              fontSize: 11,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Hey dude! Even dead I\'m the hero. Love you 3000 guys.',
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black54,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Text('data'),
+          ),
+          Container(
+            child: Text('data'),
+          ),
+          Container(
+            child: Text('data'),
+          ),
+        ],
       ),
     );
   }
