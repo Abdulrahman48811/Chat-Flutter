fb069eed06d0e33b7a584c3b1c2f748150318063
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-06 23:46:37 -0700

    onClick to chatscreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_flutter/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_flutter/models/message_model.dart';
 
@@ -31,110 +32,118 @@ class HomeScreen extends StatelessWidget {
         itemCount: chats.length,
         itemBuilder: (BuildContext context, int index) {
           final Message chat = chats[index];
-          return Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 15,
+          return GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ChatScreen(),
+              ),
             ),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(2),
-                  decoration: chat.unread
-                      ? BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(40)),
-                          border: Border.all(
-                            width: 2,
-                            color: Colors.teal,
-                          ),
-                          // shape: BoxShape.circle,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 15,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: chat.unread
+                        ? BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.teal,
                             ),
-                          ],
-                        )
-                      : BoxDecoration(
-                          shape: BoxShape.circle,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                            ),
-                          ],
-                        ),
-                  child: CircleAvatar(
-                    radius: 35,
-                    backgroundImage: AssetImage(chat.sender.imageUrl),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.65,
-                  padding: EdgeInsets.only(
-                    left: 20,
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Row(
-                            children: [
-                              Text(
-                                chat.sender.name,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            // shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
                               ),
-                              chat.sender.isOnline
-                                  ? Container(
-                                      margin: const EdgeInsets.only(
-                                        left: 5,
-                                      ),
-                                      width: 7,
-                                      height: 7,
-                                      decoration: BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.amber),
-                                    )
-                                  : Container(
-                                      child: null,
-                                    ),
+                            ],
+                          )
+                        : BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                              ),
                             ],
                           ),
-                          Text(
-                            chat.time,
+                    child: CircleAvatar(
+                      radius: 35,
+                      backgroundImage: AssetImage(chat.sender.imageUrl),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    padding: EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: [
+                                Text(
+                                  chat.sender.name,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                chat.sender.isOnline
+                                    ? Container(
+                                        margin: const EdgeInsets.only(
+                                          left: 5,
+                                        ),
+                                        width: 7,
+                                        height: 7,
+                                        decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.amber),
+                                      )
+                                    : Container(
+                                        child: null,
+                                      ),
+                              ],
+                            ),
+                            Text(
+                              chat.time,
+                              style: TextStyle(
+                                fontSize: 11,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            chat.text,
                             style: TextStyle(
-                              fontSize: 11,
-                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
                               color: Colors.black54,
                             ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
                           ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          chat.text,
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.black54,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
