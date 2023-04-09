013c0eacce56332e6cccf0b5d2f725d96e9b88ea
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-08 23:39:04 -0700

    fixed header

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,7 +14,37 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.user.name),
+        brightness: Brightness.dark,
+        centerTitle: true,
+        title: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: widget.user.name,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              TextSpan(text: "\n"),
+              TextSpan(
+                text: 'Online',
+                style: TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
       body: Container(
         child: Text('data'),
